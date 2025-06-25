be2800e5f2de0aa7042d5b2110db0f2789d7b8cc
sookyung <sookyung963@naver.com>
2025-06-25 10:24:38 +0900

    what

---
+++ src/test/java/com/example/cicd_docker/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cicd_docker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+
+    }
+}
+
