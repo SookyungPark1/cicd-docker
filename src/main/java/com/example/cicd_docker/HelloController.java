6b99406a595e21c7178f8ae3b627ab40676e64f5
sookyung <sookyung963@naver.com>
2025-06-25 11:05:02 +0900

    please

--- src/main/java/com/example/cicd_docker/HelloController.java
+++ src/main/java/com/example/cicd_docker/HelloController.java
@@ -3,8 +3,9 @@ package com.example.cicd_docker;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @GetMapping("/hi")
     public String hi(){
