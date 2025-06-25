d8162d16d08042299543dc1583375d3e963d0869
sookyung <sookyung963@naver.com>
2025-06-25 10:40:39 +0900

    please

--- src/main/java/com/example/cicd_docker/HelloController.java
+++ src/main/java/com/example/cicd_docker/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/hi")
     public String hi(){
-        return "hi   ^^";
+        return "hi please";
     }
 
 }
